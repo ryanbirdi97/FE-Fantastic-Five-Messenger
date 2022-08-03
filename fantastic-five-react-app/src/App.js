2dc5d2763f9983248fc0c4ab222b4eba4f8798e9
Ph <ph@Hs-MacBook-Pro.lan>
2022-08-03 12:01:02 +0100

    chatroom component created

--- fantastic-five-react-app/src/App.js
+++ fantastic-five-react-app/src/App.js
@@ -1,10 +1,16 @@
 import Users from "./components/Users.js";
-
-
+import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
+import Chatroom from "./components/Chatroom.js";
 function App() {
   return (
     <div className="App">
-      <Users />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/chatroom" element={<Chatroom />} />
+        </Routes>
+      </BrowserRouter>
+
       <header className="App-header"></header>
     </div>
   );
--- fantastic-five-react-app/src/components/Users.js
+++ fantastic-five-react-app/src/components/Users.js
@@ -1,10 +1,38 @@
 import { fetchUsers } from "../api.js";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Users() {
+  const [users, setUsers] = useState([]);
+  const [user2, setUser2] = useState("");
+
+  useEffect(() => {
+    fetchUsers().then((users) => {
+      setUsers(users);
+    });
+  }, []);
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate("/chatroom");
+    console.log("second useeffect inside Users.js");
+  }, [user2]);
+
   return (
     <div className="Users">
-      {fetchUsers().then((users) => {
-        // console.log(users);
+      {users.map((user) => {
+        return (
+          <>
+            <h1> {user.username} </h1>
+            <button
+              onClick={(e) => {
+                setUser2(user.username);
+              }}
+            >
+              {" "}
+              Select{" "}
+            </button>
+          </>
+        );
       })}
     </div>
   );
